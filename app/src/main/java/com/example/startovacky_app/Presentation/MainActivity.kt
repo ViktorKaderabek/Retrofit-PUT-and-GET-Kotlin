475b4f9d454e23f1985fb882899f839ed36a49ec
viktorkaderabek <viktorkaderabekpc@seznam.cz>
2021-12-19 12:28:01 +0100

    Projekt

--- app/src/main/java/com/example/startovacky_app/Presentation/MainActivity.kt
+++ app/src/main/java/com/example/startovacky_app/Presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.startovacky_app.Presentation
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
@@ -37,7 +38,7 @@ class MainActivity : AppCompatActivity() {
             mainViewModel.loadItems()
 
         }
-        binding.floatingActionButton.setOnClickListener {
+        binding.btnNote.setOnClickListener {
 
             CoroutineScope(Dispatchers.IO).launch {
                 mainViewModel.createData()
@@ -47,5 +48,90 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+
+        binding.btnAbc.setBackgroundColor(Color.parseColor("#bdbdbd"))
+        binding.btnLetovo.setTextColor(Color.parseColor("#3da142"))
+        binding.btnNejblize.setBackgroundColor(Color.WHITE)
+        binding.btnLetovo.setBackgroundColor(Color.WHITE)
+        binding.btnZeme.setBackgroundColor(Color.WHITE)
+        binding.cardViewAbc.cardElevation = 10F
+        binding.cardViewNejblize.cardElevation = 0F
+        binding.cardViewLetovo.cardElevation = 0F
+        binding.cardViewZeme.cardElevation = 0F
+
+        binding.btnMista.setBackgroundColor(Color.RED)
+        binding.btnMista.setTextColor(Color.WHITE)
+        binding.btnStartovacky.setBackgroundColor(Color.WHITE)
+        binding.btnStartovacky.setTextColor(Color.BLACK)
+        binding.cardViewStartovacky.cardElevation = 10F
+        binding.cardViewMista.cardElevation = 0F
+
+        binding.btnMista.setOnClickListener {
+
+            binding.btnMista.setBackgroundColor(Color.RED)
+            binding.btnMista.setTextColor(Color.WHITE)
+            binding.btnStartovacky.setBackgroundColor(Color.WHITE)
+            binding.btnStartovacky.setTextColor(Color.BLACK)
+            binding.cardViewStartovacky.cardElevation = 10F
+            binding.cardViewMista.cardElevation = 0F
+        }
+        binding.btnStartovacky.setOnClickListener {
+
+            binding.btnStartovacky.setBackgroundColor(Color.RED)
+            binding.btnStartovacky.setTextColor(Color.WHITE)
+            binding.btnMista.setBackgroundColor(Color.WHITE)
+            binding.btnMista.setTextColor(Color.BLACK)
+            binding.cardViewStartovacky.cardElevation = 0F
+            binding.cardViewMista.cardElevation = 10F
+        }
+        binding.btnAbc.setOnClickListener {
+
+            binding.btnAbc.setBackgroundColor(Color.parseColor("#bdbdbd"))
+            binding.btnLetovo.setTextColor(Color.parseColor("#3da142"))
+            binding.btnNejblize.setBackgroundColor(Color.WHITE)
+            binding.btnLetovo.setBackgroundColor(Color.WHITE)
+            binding.btnZeme.setBackgroundColor(Color.WHITE)
+            binding.cardViewAbc.cardElevation = 10F
+            binding.cardViewNejblize.cardElevation = 0F
+            binding.cardViewLetovo.cardElevation = 0F
+            binding.cardViewZeme.cardElevation = 0F
+        }
+        binding.btnNejblize.setOnClickListener {
+
+            binding.btnAbc.setBackgroundColor(Color.WHITE)
+            binding.btnNejblize.setBackgroundColor(Color.parseColor("#bdbdbd"))
+            binding.btnLetovo.setBackgroundColor(Color.WHITE)
+            binding.btnLetovo.setTextColor(Color.parseColor("#3da142"))
+            binding.btnZeme.setBackgroundColor(Color.WHITE)
+            binding.cardViewAbc.cardElevation = 0F
+            binding.cardViewNejblize.cardElevation = 10F
+            binding.cardViewLetovo.cardElevation = 0F
+            binding.cardViewZeme.cardElevation = 0F
+        }
+        binding.btnLetovo.setOnClickListener {
+
+            binding.btnAbc.setBackgroundColor(Color.WHITE)
+            binding.btnNejblize.setBackgroundColor(Color.WHITE)
+            binding.btnLetovo.setBackgroundColor(Color.parseColor("#3da142"))
+            binding.btnLetovo.setTextColor(Color.parseColor("#3da142"))
+            binding.btnZeme.setBackgroundColor(Color.WHITE)
+            binding.cardViewAbc.cardElevation = 0F
+            binding.cardViewNejblize.cardElevation = 0F
+            binding.cardViewLetovo.cardElevation = 10F
+            binding.cardViewZeme.cardElevation = 0F
+        }
+        binding.btnZeme.setOnClickListener {
+
+            binding.btnAbc.setBackgroundColor(Color.WHITE)
+            binding.btnNejblize.setBackgroundColor(Color.WHITE)
+            binding.btnLetovo.setBackgroundColor(Color.WHITE)
+            binding.btnLetovo.setTextColor(Color.parseColor("#3da142"))
+            binding.btnZeme.setBackgroundColor(Color.parseColor("#bdbdbd"))
+            binding.cardViewAbc.cardElevation = 0F
+            binding.cardViewNejblize.cardElevation = 0F
+            binding.cardViewLetovo.cardElevation = 0F
+            binding.cardViewZeme.cardElevation = 10F
+        }
     }
+
 }
