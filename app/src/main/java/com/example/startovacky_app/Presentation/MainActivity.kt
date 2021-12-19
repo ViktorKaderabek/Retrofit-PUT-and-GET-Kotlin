709adab53af5a2595754d274eda4c2ac1477a386
viktorkaderabek <viktorkaderabekpc@seznam.cz>
2021-12-19 15:44:15 +0100

    Projekt

--- app/src/main/java/com/example/startovacky_app/Presentation/MainActivity.kt
+++ app/src/main/java/com/example/startovacky_app/Presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.startovacky_app.Presentation
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
 import androidx.appcompat.app.AppCompatActivity
@@ -11,14 +12,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.startovacky_app.Presentation.ViewModels.MainViewModel
 import com.example.startovacky_app.R
 import com.example.startovacky_app.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var mainViewModel: MainViewModel
+    private var CR_running: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +36,20 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerview.setHasFixedSize(true)
 
         CoroutineScope(Dispatchers.IO).launch {
+
             mainViewModel.loadItems()
 
+            withContext(Dispatchers.Main){
+
+                runBlocking {
+
+                 delay(5000)
+                }
+
+                binding.progressBar.visibility = View.GONE
+            }
         }
+
         binding.floatingActionButton.setOnClickListener {
 
             CoroutineScope(Dispatchers.IO).launch {
